982adeaaf4930e2462f7dc877e4f331e7e5dcde7
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-04-06 14:41:35 -0500

    added basic structure in files, no implicit any in ts config

---
+++ src/app/signup.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./layout.module.scss";
+
+export default function Home() {
+  return (
+    <main>
+      <div className={styles.container}>
+        <h1 className={styles.text}>signup page</h1>
+      </div>
+    </main>
+  );
+}
